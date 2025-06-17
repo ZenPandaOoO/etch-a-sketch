87e9f30f0367b75add6a023afc997ee617da4110
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-06-17 09:28:39 -0400

    created colorDive and setColor

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(32);
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
     console.log("Hello");
 })
 
 function createBoard(size){
     let board = document.querySelector(".board");
 
-    board.style.gridTemplateColumns = "repeat(${size}, 1fr)";
-    board.style.gridTemplateRows = "repeat(${size}, 1fr)";
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let numDivs = size * size;
 
     for(let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv);
         board.insertAdjacentElement("beforeend", div);
     }
 }
+
+function getSize(){
+    let input = prompt("Size of the Board");
+    let message = document.querySelector("#message");
+    if (input == ""){
+        message.innerHTML = "Please provide a number";
+    } else if (input < 0 || input > 100) {
+        message.innerHTML = "Provide a number between 1 and 100";
+    } else {
+        message.innerHTML = "Draw!";
+        return input;
+    }
+}
+
+function colorDiv(){
+    if (color == 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = "black";
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+//Things to look up
+//GridTemplateColumns/Rows
+//HSL Color randomiser
+//innerHTML
+//insertAdjacentElement
+//CSS display GRID
